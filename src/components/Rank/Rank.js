f3e10cecbd98f7fdd9a52f9e49de8fd40d818b70
Darius Seals <tristarmediagroupinc@gmail.com>
2019-03-30 00:55:15 -0400

    initial commit.  created front end of app by added logo, form and button with styling

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Darius, Your current rank is ...'}
+      </div>
+      <div className='white f1'>
+        {'#1'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
